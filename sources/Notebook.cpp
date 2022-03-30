8fd84cc11b30623a85fab53daa83645c17377c7f
saeedSAEED1799 <73116797+SAEED1799@users.noreply.github.com>
2022-03-30 07:04:31 -0400

    yes-good

--- sources/Notebook.cpp
+++ sources/Notebook.cpp
@@ -13,26 +13,19 @@ namespace ariel {
     typedef  int uint;
     typedef  long ulong;
 
-    void checkThrow(int page, int row, int column, Direction Dir, int count)
-    {
-        const int num = 100;
-        if (Dir == Direction::Horizontal && (count >= num))
-        {
-            throw invalid_argument("1 In each line there is 100 char please dont pass it");
-        }
-        if (Dir == Direction::Vertical && column >= num)
-        {
-            throw invalid_argument("2 In each line there is 100 char please dont pass it");
-        }
-        if (page < 0 || row < 0 || column < 0 || count < 0)
-        {
-            throw invalid_argument("You entered a negative index");
-        }
-    }
-
     void Notebook::write(int page,  int row, int column, Direction direction, const std::string &message) {
-        checkThrow(page, row, column, direction, column + (int)message.length());
-
+         if (page < 0 || row < 0 || column < 0 || column + (int)message.length()< 0)
+        {
+            throw invalid_argument("you have a negative index");
+        }
+        if (direction == Direction::Vertical && column >= maxSym)
+        {
+            throw invalid_argument("you have more than 100 lines");
+        }
+         if (direction == Direction::Horizontal && (column + (int)message.length() >= maxSym))
+        {
+            throw invalid_argument("you have more than 100 to read");
+        }
         int length = message.length();
         if (length == 0) {
             return ;
@@ -65,14 +58,14 @@ namespace ariel {
             if (bound.size_y < size_y) {
                 bound.size_y = size_y;
             }
-            for (int i = 0; i < length; i++) {
-                int x = column + i;
-                if(checkCharAt(page, x, row)){
-//                    throw invalid_argument("error");
-                    return;
-                }
+//             for (int i = 0; i < length; i++) {
+//                 int x = column + i;
+// //                 if(checkCharAt(page, x, row)){
+// // //                    throw invalid_argument("error");
+// //                     return;
+// //                 }
 
-            }
+         //   }
         } else {
             ulong size_y = length;
             ulong length_1 = 1;
@@ -99,16 +92,16 @@ namespace ariel {
             if (bound.size_y < size_y) {
                 bound.size_y = size_y;
             }
-            for (int i = 0; i < length; i++) {
-                int y = row + i;
-                if(checkCharAt(page, column, y)){
-//                    throw invalid_argument("error");
-                    return;
-                }
+//             for (int i = 0; i < length; i++) {
+//                 int y = row + i;
+// //                 if(checkCharAt(page, column, y)){
+// // //                    throw invalid_argument("error");
+// //                     return;
+// //                 }
 
-            }
+//             }
+//         }
         }
-
         if (direction == Direction::Horizontal) {
             ulong size_y = 1;
             if (column > bound.min_x) {
@@ -146,11 +139,11 @@ namespace ariel {
             for(auto str : message){
                 int x = column + len;
                 //||(str<=maximum&&str>=minimum)
-                if((noteBook[{page, {row, x}}].d!='_')||(noteBook[{page, {row, x}}].d=='~')||(str=='~')||!(str<=maximum&&str>=minimum))
+                if((noteBook[page][row][x].d!='_')||(noteBook[page][row][x].d=='~')||(str=='~')||!(str<=maximum&&str>=minimum))
                 {
                     throw invalid_argument("no write");
                 }
-                noteBook[{page, {row, x}}].d = str;
+                noteBook[page][row][x].d = str;
                 len++;
             }
 
@@ -191,20 +184,32 @@ namespace ariel {
             const int minimum = 32;
             for(auto str : message){
                 int x = row + len;
-                if((noteBook[{page, {x, column}}].d!='_')||(noteBook[{page, {x, column}}].d=='~')||(str=='~')||!(str<=maximum&&str>=minimum))
+                if((noteBook[page][x][column].d!='_')||(noteBook[page][x][column].d=='~')||(str=='~')||!(str<=maximum&&str>=minimum))
                 {
                     throw invalid_argument("no write");
                 }
-                noteBook[{page, {x, column}}].d = str;
+                noteBook[page][x][column].d = str;
                 len++;
             }
 
         }
     }
 
-    std::string Notebook::read( int page, int row, int column, Direction direction, int length) const {
+    string Notebook::read( int page, int row, int column, Direction direction, int length) {
         
-        checkThrow(page, row, column, direction, (column + length) - 1);
+       
+         if (page < 0 || row < 0 || column < 0 || (column + length) - 1 < 0)
+        {
+            throw invalid_argument("you have a negative index");
+        }
+        if (direction == Direction::Vertical && column >= maxSym)
+        {
+            throw invalid_argument("you have more than 100 lines");
+        }
+         if (direction == Direction::Horizontal && ((column + length) - 1 >= maxSym))
+        {
+            throw invalid_argument("you have more than 100 to read");
+        }
 
         if (length == 0) {
             return "";
@@ -216,20 +221,13 @@ namespace ariel {
         string message;
         if (direction == Direction::Horizontal) {
             for (int i = 0; i < length; i++) {
-                int x = column + i;
-                pair< int, pair< int,  int>> key = make_pair(page, make_pair(row, x));
-                if (noteBook.count(key) == 1) {
-                    message += noteBook.at(key).d;
-                }
+                int x = column + i;            
+                message += noteBook[page][row][x].d;
             }
         } else {
             for (int i = 0; i < length; i++) {
                 int y = row + i;
-                pair< int, pair< int,  int>> key = make_pair(page, make_pair(y, column));
-                if (noteBook.count(key) == 1) {
-                    message += noteBook.at(key).d;
-                }
-
+                message += noteBook[page][y][column].d;
             }
         }
 
@@ -238,8 +236,7 @@ namespace ariel {
 
     void Notebook::show(int pages) const {
         if(pages<=0){
-           throw invalid_argument("error");
- 
+           throw invalid_argument("error-you have to give apostive num of page");
         }
         if (bound.size_x == 0) {
             return;
@@ -249,15 +246,29 @@ namespace ariel {
             while (space.length() < to_string(bound.min_y + bound.size_y + 2).length() + 2) {
                 space += " ";
             }
-            if(!(bound.min_y + i - 1<0&&bound.min_x - 2<0)){
-              // cout << space << read(pages, bound.min_y + i - 1, bound.min_x - 2, Direction::Horizontal, bound.size_x + 4) << endl;
+            // if(!(bound.min_y + i - 1<0&&bound.min_x - 2<0)){
+            //   cout << space << read(pages, bound.min_y + i - 1, bound.min_x - 2, Direction::Horizontal, bound.size_x + 4) << endl;
 
-            }
+            // }
+            // cout <<read( pages, 0, 0, Direction::Horizontal, 100)<< endl;
+
         }
     }
 
     void Notebook::erase( int page,  int row, int column, Direction direction,int length) {
-        checkThrow(page, row, column, direction, (column + length) );
+         if (page < 0 || row < 0 || column < 0 || (column + length)  < 0)
+        {
+            throw invalid_argument("you have a negative index");
+        }
+        if (direction == Direction::Vertical && column >= maxSym)
+        {
+            throw invalid_argument("you have more than 100 lines");
+        }
+         if (direction == Direction::Horizontal && ((column + length) >= maxSym))
+        {
+            throw invalid_argument("you have more than 100 to read");
+        }
+
         if (length == 0) {
             // do nothing
             return;
@@ -265,76 +276,18 @@ namespace ariel {
         if(direction == Direction::Horizontal && column + length > maxSym){
             cout << "throw invalid_argument(\"error\")" << endl;
         }
-
-
+        char del = '~';
         if (direction == Direction::Horizontal) {
-            ulong size_y = 1;
-            if (column > bound.min_x) {
-                length += column - bound.min_x;
-            }
-
-            // same but for the y axis.
-            if (row > bound.min_y) {
-                size_y += row - bound.min_y;
-            }
-            if (bound.min_x > column) {
-                bound.min_x = column;
-            }
-
-            if (bound.min_y > row) {
-                bound.min_y = row;
-            }
-
-            if (bound.size_x < length) {
-                bound.size_x = length;
-            }
-
-            if (bound.size_y < size_y) {
-                bound.size_y = size_y;
-            }
-            for (int i = 0; i < length; i++) {
-                int  x = column + i;
-                noteBook[{page, {x, row}}].d = '~';
-            }
-        } else {
-            ulong size_y = length;
-            ulong length_1 = 1;
-            if (column > bound.min_x) {
-                length_1 += column - bound.min_x;
-            }
-
-            // same but for the y axis.
-            if (row > bound.min_y) {
-                size_y += row - bound.min_y;
-            }
-            if (bound.min_x > column) {
-                bound.min_x = column;
-            }
-
-            if (bound.min_y > row) {
-                bound.min_y = row;
-            }
-
-            if (bound.size_x < length_1) {
-                bound.size_x = length_1;
-            }
-
-            if (bound.size_y < size_y) {
-                bound.size_y = size_y;
-            }
-            for (int i = 0; i < length; i++) {
-                int y = row + i;
-                noteBook[{page, {column, y}}].d = '~';
+            for(int i = column; i<column+length; i++){
+                noteBook[page][row][i].d = del;
             }
         }
+        else
+            for(int i = row; i<row+length; i++){
+                noteBook[page][i][column].d = del;
+            }
+        
     }
-    bool Notebook::checkCharAt( int page,  int x,  int y) {
-        pair< int, pair< int,  int>> key = make_pair(page, make_pair(x, y));
 
-        if (noteBook.count(key) != 1) {
-            //return true;
-        }
-        return false;
-    }
 }
 
